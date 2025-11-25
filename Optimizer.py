c7a0996599acd9d8e572ecd6afbe924a9c94c5b8
JMarcosCGomes <joaomarcoscgomes@gmail.com>
2025-11-24 22:20:18 -0300

    refactor(Optimizer): Overhaul Objective Function and Constraints - Refactor Objective Function: The objective function now utilizes heliocentric specific mechanical energy and the probe-to-target planet distance instead of the previous final velocity approach. - New Feature: Added run_simulation_if_needed helper function to conditionally execute the simulation for performance gains. - New Constraint: Implemented planet_collision_constraint. - Fix Constraint: Corrected logic in the existing dv_constraint. - Code Structure: Structural reorganization, including variable scope adjustments and general cleanup, to improve maintainability.

--- Optimizer.py
+++ Optimizer.py
@@ -4,88 +4,125 @@ from scipy.optimize import minimize
 
 class Optimizer:
 
-    def __init__(self, max_dv=5e3, initial_guess=[0.0, 0.0]):
+    def __init__(self, planet_angle_deg=0, max_dv=5e3, initial_guess=[0.0, 0.0]):
         self.max_dv = max_dv #integer / float 
         self.initial_guess = initial_guess #[guess_dvx, guess_dvy]
-        self.set_constraints()
-        self.the_fixed_part()
         self.optimization_attempts = 0
+        #cache
         self.last_params = None
-        self.last_y_full = None #i think i can substitute for sol2
-
-
-    def the_fixed_part(self):
-        self.universo = Universo()
-        self.probe_vx_index, self.probe_vy_index = self.universo.get_probe_v_indexes()
+        self.last_y_full = None 
+        #universe setup
+        self.universo = Universo(planet_angle_deg=planet_angle_deg)
         self.sol1 = self.universo.run_until_aphelion()
-        
+        self.post_aphelion_y = self.sol1.y[:, -1].copy()
+        #constraints
+        self.set_constraints()
 
 
-    #objective function, to optimize the speed
-    def objective(self, params):
+    def run_simulation_if_needed(self, params):
+        if (self.last_params is not None) and (np.allclose(params, self.last_params, atol=1e-12, rtol=0)):
+            return self.last_y_full
         dvx, dvy = params
-        post_aphelion_y = self.sol1.y[:, -1].copy()
-        post_aphelion_y[self.probe_vx_index] += dvx
-        post_aphelion_y[self.probe_vy_index] += dvy
+        post_aphelion_y = self.post_aphelion_y.copy()
+        post_aphelion_y[(self.universo.probe_index-1)*4+2] += dvx
+        post_aphelion_y[(self.universo.probe_index-1)*4+3] += dvy
 
-        sol2 = self.universo.run_after_aphelion(new_y0=post_aphelion_y)
-        y_full = np.concatenate((self.sol1.y, sol2.y), axis=1)
-        final_y = y_full[:, -1].copy()
-        probe_final_vx = final_y[self.probe_vx_index]
-        probe_finaL_vy = final_y[self.probe_vy_index]
-        final_velocity = np.sqrt(probe_final_vx**2 + probe_finaL_vy**2)
-
-        #CACHE TO SUN CONSTRAINT
+        #preciso dele pra pegar a distancia do pos aphelion
+        self.sol2 = self.universo.run_after_aphelion(new_y0=post_aphelion_y)
+        y_full = np.concatenate((self.sol1.y, self.sol2.y), axis=1)
         self.last_params = params.copy()
         self.last_y_full = y_full
-        # if its taking too long, i can change to sol2 part
+        return y_full
 
+
+    def objective(self, params):
+        dvx, dvy = params
         self.optimization_attempts += 1
+        y_full = self.run_simulation_if_needed(params)
+        final_y = y_full[:, -1]
+        
+        #muito feio, talvez fazer uma funcao pra isso
+        probe_final_x = final_y[(self.universo.probe_index-1)*4]
+        probe_final_y = final_y[(self.universo.probe_index-1)*4+1]
+        probe_final_vx = final_y[(self.universo.probe_index-1)*4+2]
+        probe_finaL_vy = final_y[(self.universo.probe_index-1)*4+3]
+        fixed_body_x = self.universo.corpos_celestes[self.universo.fixed_body_index].pos_x
+        fixed_body_y = self.universo.corpos_celestes[self.universo.fixed_body_index].pos_y
+
+        dx = probe_final_x - fixed_body_x
+        dy = probe_final_y - fixed_body_y
+        r_module = np.sqrt(dx**2 + dy**2)
+        v_module = np.sqrt(probe_final_vx**2 + probe_finaL_vy**2)
+        mu_fixed_body = self.universo.G * self.universo.corpos_celestes[self.universo.fixed_body_index].massa
+        #energia mecanica especifica heliocentrica
+        energy = (v_module**2)/2 - (mu_fixed_body / r_module)
+
+        #y parte 2
+        y_p2 = self.sol2.y
+        planet_id = self.universo.planet_index
+        probe_id = self.universo.probe_index
+        probe_all_x = y_p2[(probe_id-1)*4]
+        probe_all_y = y_p2[(probe_id-1)*4 + 1]
+        planet_all_x = y_p2[(planet_id-1)*4]
+        planet_all_y = y_p2[(planet_id-1)*4 + 1]
+        dists = np.sqrt((probe_all_x - planet_all_x)**2 + (probe_all_y - planet_all_y)**2)
+        minimal_distance = np.min(dists)
+
+        energy_weight = 1e-6
+        score_energy = - energy * energy_weight
+        distance_weight = 5e-5
+        score_distance = minimal_distance * distance_weight
+
+        planet_radius = self.universo.corpos_celestes[self.universo.planet_index].raio
+        come_closer_range = 3e7
+        total_distance = planet_radius + come_closer_range
+
+        if minimal_distance > total_distance:
+            score = score_distance * 12
+            report = "GOING TO PLANET"
+        else:
+            score = score_energy + (score_distance/8)
+            report = "OPTIMIZING SLINGSHOT"
+
         print(f"attempt: {self.optimization_attempts}")
         print(f"dvx: {dvx}")
         print(f"dvy: {dvy}")
-        print(f"final_v: {final_velocity}")
+        print(f"energy: {energy}")
+        print(f"Minimal distance: {minimal_distance}")
+        print(f"Report: {report} | Score: {score}")
         print("------------------")
-        #as far as I know, scipy can only minimize
-        #I can use as score the final_velocity OR final_velocity - penalization_to_dv_use
-        return -final_velocity
+        return score
 
 
     def dv_constraint(self, params):
-        return self.max_dv - (abs(params[0]) + abs(params[1]))
+        return self.max_dv - np.linalg.norm(params)
     
 
-    def sun_distance_constraint(self, params):
+    def planet_collision_constraint(self, params):
+        planet_id = self.universo.planet_index
+        probe_id = self.universo.probe_index
 
-        if (self.last_params is None):
-            dvx, dvy = params
-            post_aphelion_y = self.sol1.y[:, -1].copy()
-            post_aphelion_y[self.probe_vx_index] += dvx
-            post_aphelion_y[self.probe_vy_index] += dvy
-            sol2 = self.universo.run_after_aphelion(new_y0=post_aphelion_y)
-            self.last_y_full = np.concatenate((self.sol1.y, sol2.y), axis=1)
-            self.last_params = params.copy()
+        y_full = self.run_simulation_if_needed(params)
 
-        probe_x_index = self.probe_vx_index - 2
-        probe_y_index = self.probe_vy_index - 2
-        probe_all_x_positions = self.last_y_full[probe_x_index]
-        probe_all_y_positions = self.last_y_full[probe_y_index]
-        dist = np.sqrt(probe_all_x_positions**2 + probe_all_y_positions**2)
+        probe_all_x = y_full[(probe_id-1)*4]
+        probe_all_y = y_full[(probe_id-1)*4 + 1]
+        planet_all_x = y_full[(planet_id-1)*4]
+        planet_all_y = y_full[(planet_id-1)*4 + 1]
+        dist = np.sqrt((probe_all_x - planet_all_x)**2 + (probe_all_y - planet_all_y)**2)
         minimal_distance_found = np.min(dist)
-        minimal_distance_required = 0.31 * 1.496e11 #or 0.25 * 
-        #if minimal_distance_found is lower than minimal_distance required. The condition isn't satisfied; mdf >= mdr --> mdf - mdr >= 0
-        return minimal_distance_found - minimal_distance_required
+        planet_radius = self.universo.corpos_celestes[self.universo.planet_index].raio
+        safety_margin = 2e6
+        
+        return minimal_distance_found - (planet_radius + safety_margin)
 
 
     def set_constraints(self):
-        #self.constraint = ({'type': 'ineq', 'fun': self.dv_constraint},) 
         constraint_dv = {'type': 'ineq', 'fun': self.dv_constraint}
-        constraint_sun_dist = {'type': 'ineq', 'fun': self.sun_distance_constraint}
-        self.constraints = (constraint_dv, constraint_sun_dist)
+        constraint_planet_collision = {'type': 'ineq', 'fun': self.planet_collision_constraint}
 
+        self.constraints = (constraint_dv, constraint_planet_collision)
 
-    def optimize(self, maxiter=120, ftol=1e-3): 
-        #method = 'SLSQP'
+    def optimize(self, maxiter=120, ftol=1e-4): 
         #methods_to_test = ['SLSQP', 'trust-constr']
         methods_to_test = ['SLSQP']
         
@@ -94,6 +131,7 @@ class Optimizer:
             'maxiter': maxiter,
             'ftol': ftol,
             'disp': True,
+            'eps': 4.0,
         }
 
         options_trustconstr = {
@@ -116,31 +154,21 @@ class Optimizer:
             print("VVVVVVVVVVVVVVVVVVV")
 
             res = minimize(self.objective, self.initial_guess, method=method, bounds=bounds, constraints=self.constraints, options=options)
-            final_velocity = -res.fun
-            results[method] = (res.x, final_velocity)
+            final_score = -res.fun
+            results[method] = (res.x, final_score)
 
             print("==============================================================")
-            print(f"METHOD: {method} | best_dv = {res.x}, final_velocity = {final_velocity}")
+            print(f"METHOD: {method} | best_dv = {res.x}, final_velocity = {final_score}")
             print("==============================================================")
 
             self.optimization_attempts = 0
 
         print("================= SUMMARY =================")
-        for method, (dv, fv) in results.items():
-            print(f"{method}: dv={dv}, final_velocity={fv}")
+        for method, (dv, fs) in results.items():
+            print(f"{method}: dv={dv}, final_score={fs}")
         print("================= BEST METHOD =================") 
-        best_method = max(results.items(), key=lambda kv: kv[1][1]) #[1][1] eh final_velocity
+        best_method = max(results.items(), key=lambda kv: kv[1][1]) #[1][1] eh final_value
         print(f"BEST METHOD FOUND: {best_method[0]}")
         
         self.best_dv = best_method[1][0]
         return self.best_dv
-
-
-"""
-self.best_dv = res.x #dvx, dvy
-final_velocity = -res.fun
-print(self.best_dv)
-print(final_velocity)
-return self.best_dv
-"""
-
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from Universo import Universo
 from Optimizer import Optimizer
 
-## ===  AVOID to use this version for now, i'm not sure if its working correctely===
+PLANET_ANGLE_DEG = -50
+MAX_DV = 4e3
+
 if __name__ == "__main__":
-    #universo = Universo(planet_angle_deg=-125, intervalo_animacao=5)
-    universo = Universo(planet_angle_deg=-50, intervalo_animacao=5)
-    optimizer = Optimizer(max_dv=4e3, initial_guess=[0.0, 0.0])
+    universo = Universo(planet_angle_deg=PLANET_ANGLE_DEG, intervalo_animacao=5)
+    optimizer = Optimizer(planet_angle_deg=PLANET_ANGLE_DEG, max_dv=MAX_DV, initial_guess=[0.0, 0.0])
 
     # so pra ver as posicoes
     #teste = universo.simple_plot()
@@ -16,18 +17,30 @@ if __name__ == "__main__":
 
     #with "manual" optimization, to visualize
     #dv = [0.0, 0.0]
-    #dv = [4868, -128]
+    #dv = [2985, 765]
     #sol = universo.simular_optimized(dv)
     #universo.animar(sol)
 
 
     #with optimization, "reduce" max_steps from blabla/4 to blabla
-    #dv = optimizer.optimize(maxiter=40)
-    #sol = universo.simular_optimized(dv)
-    #universo.animar(sol)
+    dv = optimizer.optimize(maxiter=30)
+    sol = universo.simular_optimized(dv)
+    universo.animar(sol)
+
 
 """
-Acho que ainda ta tentando fazer slingshot com o sol mesmo eu restringindo, precisa ser mais
-nao tenho certeza se o universo ta certo, tem que verificar
-acho que corrigi a posicao e velocidade pro probe
+Universo - get_solveivp_params  - max_step = 86400
+PLANET_ANGLE_DEG = -50 
+
+attempt: 89
+dvx: 2985.0822615220627
+dvy: 765.9270087066627
+energy: 285229758.7288369
+Minimal distance: 18047242.278514884
+Report: OPTIMIZING SLINGSHOT | Score: -194.99354733626245
 """
+
+#TODO URGENTE, CONSERTA MAX_STEP, bota que nem max_dv e planet_angledeg, passa como parametro pro universo (se n tiver, max_step fica 1 dia mesmo)
+#TODO Menos urgente, aumenta o peso, pelos logs recentes o otimizador toda hora ta saindo de perto da terra
+#TODO Igualmente urgente, acho que é o eps mt alto, acho que os pesos ja fazem o bastante
+#TODO Testar métodos diferentes, mesmo achando que o SLSQP é melhor pra esse caso
